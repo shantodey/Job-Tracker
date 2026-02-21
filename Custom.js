675e8bb6f991c00657e49f201b1394b3064c6d98
shantodey <ratul3330@gmail.com>
2026-02-21 21:00:38 +0600

    basic desgine compalte

---
+++ Custom.js
@@ -0,0 +1,31 @@
+// function togotl(id){
+//     const addmoney=document.getElementById('addMoney')
+//     const cashout=document.getElementById('cashOut')
+//     const transition__history=document.getElementById('transition__history')
+//     const tnsMoney=document.getElementById('tnsMoney')
+//     const getbonus=document.getElementById('getbonus')
+//     const paybill=document.getElementById('paybill')
+
+//     addmoney.classList.add('hidden')
+//     cashout.classList.add('hidden')
+//     transition__history.classList.add('hidden')
+//     tnsMoney.classList.add('hidden')
+//     getbonus.classList.add('hidden')
+//     paybill.classList.add('hidden')
+
+//     const selected =document.getElementById(id);
+//     selected.classList.remove('hidden')
+// }
+
+const rejectedBtns = document.querySelectorAll('.Rejected');
+for (const button of rejectedBtns) {
+    button.addEventListener('click', function() {
+        alert('Rejected!');
+    });
+}
+const rejectedBtns = document.querySelectorAll('.Rejected');
+for (const button of rejectedBtns) {
+    button.addEventListener('click', function() {
+        alert('Rejected!');
+    });
+}
