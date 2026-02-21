1ef133b2ad8eb990ba6ca060884ad69f3a8ee35d
shantodey <ratul3330@gmail.com>
2026-02-21 22:15:07 +0600

    togole button active

--- Custom.js
+++ Custom.js
@@ -1,31 +1,60 @@
-// function togotl(id){
-//     const addmoney=document.getElementById('addMoney')
-//     const cashout=document.getElementById('cashOut')
-//     const transition__history=document.getElementById('transition__history')
-//     const tnsMoney=document.getElementById('tnsMoney')
-//     const getbonus=document.getElementById('getbonus')
-//     const paybill=document.getElementById('paybill')
+// count of total job 
+const card__container=document.getElementById('card__container').children.length;
+const stattotal =document.getElementById('stat-total');
+const stat_interview =document.getElementById('stat-interview');
+const stat_rejected =document.getElementById('stat-rejected');
+stattotal.innerText=card__container
 
-//     addmoney.classList.add('hidden')
-//     cashout.classList.add('hidden')
-//     transition__history.classList.add('hidden')
-//     tnsMoney.classList.add('hidden')
-//     getbonus.classList.add('hidden')
-//     paybill.classList.add('hidden')
 
-//     const selected =document.getElementById(id);
-//     selected.classList.remove('hidden')
+
+
+const all=document.getElementById('all')
+const interview=document.getElementById('interview')
+const reg=document.getElementById('reg')
+function togole(id){
+    all.classList.remove('bg-blue-400', 'text-white')
+    interview.classList.remove('bg-blue-400', 'text-white')
+    reg.classList.remove('bg-blue-400', 'text-white')
+
+    const activeid=document.getElementById(id);
+    activeid.classList.add('bg-blue-400', 'text-white')
+}
+
+
+
+// const rejectedBtns = document.querySelectorAll('.Rejected');
+// for (const button of rejectedBtns) {
+//     button.addEventListener('click', function() {
+//         const rejeCted = document.createElement('button');
+//         rejeCted.className = 'btn btn-soft btn-secondary border-[#EF4444]';
+//         rejeCted.innerHTML = `
+//             Rejected
+//         `;
+//                 const card = button.closest('.card');
+//         const targetDiv = card.querySelector('.neew__button');
+//         targetDiv.appendChild(rejeCted);
+//     });
+// }
+// const interviewBtns = document.querySelectorAll('.interview');
+// for (const button of interviewBtns) {
+//     button.addEventListener('click', function() {
+//         const interView = document.createElement('button');
+//         interView.className = 'btn btn-primary';
+//         interView.innerHTML = `
+//             Primary
+//         `;
+//                 const card = button.closest('.card');
+//         const targetDiv = card.querySelector('.neew__button');
+//         targetDiv.appendChild(interView);
+//     });
 // }
 
-const rejectedBtns = document.querySelectorAll('.Rejected');
-for (const button of rejectedBtns) {
-    button.addEventListener('click', function() {
-        alert('Rejected!');
-    });
-}
-const rejectedBtns = document.querySelectorAll('.Rejected');
-for (const button of rejectedBtns) {
-    button.addEventListener('click', function() {
-        alert('Rejected!');
-    });
-}
+
+// // deleting the hole card
+// const deleteBtns = document.querySelectorAll('.delete__card');
+// for (const btn of deleteBtns) {
+//     btn.addEventListener('click', function() {
+//         const card = btn.closest('.card');
+//         card.remove();
+//     });
+// }
